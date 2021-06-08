e62cfee806168c94b2415051a24ab3169868ac7c
mngagan <mngagan@gmail.com>
2021-06-08 21:42:57 +0530

    seperated out questions to an other file

---
+++ src/data.js
@@ -0,0 +1,16 @@
+let allQues = [
+  { question: "Gold అనగా దేనిని చెప్తుంది ?" },
+  { question: "యోహాను ౩:16 (John 3:16)  లో ఏమి ఉంది ?" },
+  { question: "పాపమును ఏ రంగుతో పోలుస్తాము ?" },
+  { question: "Red color ఏమి చెప్తుంది  ?" },
+  { question: "Green color ఏమి చెప్తుంది  ?" },
+  { question: "యోహాను ౩:16 (John 3:16) ఏమి color గురించి  చెప్తుంది   ?" },
+  { question: "పరలోక పట్టణం లో 12 12 ?" },
+  { question: "పాపం చెయ్యటం వలన ఏమి పోగొట్టుకుంటున్నారు  ?" },
+  { question: "మనము ఎవరికి సమీపంగా ఉండాలి  ?" },
+  { question: "దేవుడు మనలని ఎలా ఉండమని చెప్తున్నాడు  ?" },
+  { question: "పవిత్రతకు ఏమి సూచిస్తుంది   ?" },
+  { question: "White color ఏమి చెప్తుంది  ?" }
+];
+
+export default allQues;
